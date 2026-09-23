09340b796c40b921b727b1d11efe77d3561fece3
lockwood.da88@gmail.com <lockwood.da88@gmail.com>
2026-09-23 05:21:28 +0000

    Added team pipeline

--- monday_pinball_bronze/transformations/raw_data.py
+++ monday_pinball_bronze/transformations/raw_data.py
@@ -1,4 +1,4 @@
-from utilities.extraction import html_extract
+from utilities.extraction import html_extract, team_roster_extract
 from bs4 import BeautifulSoup
 import pyspark.pipelines as dp
 import requests
@@ -20,3 +20,17 @@ def raw_stats():
     return df
 
 
+@dp.table(name="bronze.raw_team_roster")
+def raw_team_roster():
+
+    def create_df(team_initial: str):
+
+        df = spark.createDataFrame(team_roster_extract(team_initial))
+        df = df.withColumn("team_inital", team_initial)
+
+        # Assign team names based on team initial
+        if team_initial == "ADB":
+            df = df.withColumn("team_name", "Admiraballs")
+        elif team_initial == "BAD":
+            df = df.withColumn("team_name", "Bad Cats")
+        elif team_inital == "" 
--- monday_pinball_bronze/utilities/extraction.py
+++ monday_pinball_bronze/utilities/extraction.py
@@ -27,3 +27,37 @@ def html_extract(url: str):
 
     return table_data
 
+
+# Reusable function to scrape meta data for teams
+def team_roster_extract(team_inital: str):
+
+    html_src = requests.get(f"https://mondaynightpinball.com/teams/ADB").content
+    soup = BeautifulSoup(html_src, "html.parser")
+    table = soup.find(id="team_roster")
+
+    rows = table.find_all('tr')
+    # First row contains the headers
+    header_cells = rows[0].find_all(['th', 'td'])
+    headers = [cell.text.strip() for cell in header_cells]
+
+    rows = table.find_all('tr')
+
+    # First row contains the headers
+    header_cells = rows[0].find_all(['th', 'td'])
+    headers = [cell.text.strip() for cell in header_cells]
+
+    # Remaining rows are data
+    table_data = []
+
+    for row in rows[1:]:
+        cells = row.find_all('td')
+        row_dict = {headers[i]: cell.text.strip() for i, cell in enumerate(cells) if i < len(headers)}
+        table_data.append(row_dict)
+
+    return table_data
+
+    
+def team_info_extract(team_initial: str):
+
+    pass
+    pass
